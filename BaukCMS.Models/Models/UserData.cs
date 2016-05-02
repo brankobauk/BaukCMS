341d1515535af35c790f29353191ad927b417cfb
Brancinho <branko.bauk@gmail.com>
2016-05-02 17:21:53 +0200

    commitall

---
+++ BaukCMS.Models/Models/UserData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaukCMS.Models.Models
+{
+    public class UserData
+    {
+        public int UserDataId { get; set; }
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int? CompanyId { get; set; }
+        public bool Active { get; set; }
+        public int LastSiteId { get; set; }
+        public virtual Company Company { get; set; }
+        public virtual UserProfile UserProfile { get; set; }
+
+    }
+}
---
+++ BaukCMS.UI/obj/Release/Package/PackageTmp/Scripts/table.js
@@ -0,0 +1,6 @@
+﻿$(document).ready(function() {
+    $("table.grid-table tr").each(function(i) {
+        if (i % 2 === 0) $(this).addClass("even");
+    });
+    $("table.grid-table tr td:last-child").css("width", "100%").css("text-align", "right");
+});
